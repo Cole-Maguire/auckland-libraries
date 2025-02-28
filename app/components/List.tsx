283679028c57d61ce18dbbed913d39eba7f2ac26
Cole Maguire <hello@colemaguire.me>
2025-02-28 19:49:26 +1300

    sort by name

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -30,45 +30,47 @@ export function List({
 
   return (
     <div className="h-full overflow-y-auto bg-gray-300 bg-white p-4 shadow-md">
-      {libraries.map((library) => (
-        <div
-          key={library.libraryId}
-          className={`duration-400 mb-4 flex items-center justify-between rounded-lg p-4 shadow transition-all ${
-            highlightedLibrary?.libraryId === library.libraryId
-              ? "bg-red-50"
-              : "bg-gray-100"
-          }`}
-          ref={(e) => {
-            ref.current[library.libraryId] = e;
-          }}
-          onClick={() => setHighlightedLibrary(library)}
-        >
-          <div>
-            <h3 className="text-lg font-semibold">{library.name}</h3>
-            <a
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-                library.name + " Auckland",
-              )}`}
-            >
-              {library.address}
-            </a>
+      {libraries
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((library) => (
+          <div
+            key={library.libraryId}
+            className={`duration-400 mb-4 flex items-center justify-between rounded-lg p-4 shadow transition-all ${
+              highlightedLibrary?.libraryId === library.libraryId
+                ? "bg-red-50"
+                : "bg-gray-100"
+            }`}
+            ref={(e) => {
+              ref.current[library.libraryId] = e;
+            }}
+            onClick={() => setHighlightedLibrary(library)}
+          >
+            <div>
+              <h3 className="text-lg font-semibold">{library.name}</h3>
+              <a
+                className="underline"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                  library.name + " Auckland",
+                )}`}
+              >
+                {library.address}
+              </a>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                checked={library.visited}
+                className="h-6 w-6 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                onChange={async () => {
+                  await api.toggleLibrary(library);
+                  setLibraries([...libraries]); // gotta force a ref change after all of our mucky mutation
+                }}
+              />
+            </div>
           </div>
-          <div>
-            <input
-              type="checkbox"
-              checked={library.visited}
-              className="h-6 w-6 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-              onChange={async () => {
-                await api.toggleLibrary(library);
-                setLibraries([...libraries]); // gotta force a ref change after all of our mucky mutation
-              }}
-            />
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
